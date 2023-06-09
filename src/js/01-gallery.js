2a66f72be1b1312fea8168b7464ef861ac1733ac
Z1NG0D <borovskiyyaroslavv@gmail.com>
2023-06-09 18:27:02 +0200

    comit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+
+
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li>`;
+        }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a',{
+    caption:true, 
+    captionSelector: 'img',
+    captionData: 'Title',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+gallery.style.listStyle = 'none';
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function saveTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+player.on('timeupdate', throttle(saveTime, 1000));
+player.setCurrentTime(savedTime);
 
